80bb666141eea19ee42c35bc35a06dff2af2a1a7
Lionel ORCIL <orcil.lionel@gmail.com>
2024-05-20 19:40:15 +0200

    add gps

--- rhacobot_simulation/launch/gz.launch.py
+++ rhacobot_simulation/launch/gz.launch.py
@@ -52,6 +52,7 @@ def generate_launch_description():
         executable='parameter_bridge',
         arguments=['/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock',
                    '/imu@sensor_msgs/msg/Imu[gz.msgs.IMU',
+                   '/gps@sensor_msgs/msg/NavSatFix[gz.msgs.NavSat',
                    ],
         output='screen'
     )
